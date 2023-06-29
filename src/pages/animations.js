8be423630ab5512b8f3d4db4fd9d63c4c8d3b4b2
Andreisc0912 <adreisc912@gmail.com>
2023-06-29 19:33:56 +0200

    add:framer accordion component

--- src/pages/animations.js
+++ src/pages/animations.js
@@ -1,9 +1,9 @@
 import "./page.scss";
 import React, { useEffect, useState, useRef } from 'react';
 
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 
-import { Order, Menu, Transform, ScrollTrigger, Morphing } from "./components/framermotions";
+import { Order, Menu, Transform, ScrollTrigger, Morphing, Accordion } from "./components/framermotions";
 
 export default function Animations(props) {
     return (
@@ -19,6 +19,11 @@ export default function Animations(props) {
                     </Grid>
                     <Grid item>
                         <Morphing />
+                    </Grid>
+                </Grid>
+                <Grid item container direction="row" justifyContent="space-around" alignItems="center" className="mt-3">
+                    <Grid item md={4}>
+                        <Accordion />
                     </Grid>
                 </Grid>
                 <Grid item>
--- src/pages/components/framermotions/index.jsx
+++ src/pages/components/framermotions/index.jsx
@@ -3,5 +3,6 @@ import Menu from './menu';
 import Transform from './transform';
 import ScrollTrigger from './scrolltrigger';
 import Morphing from './morphing';
+import Accordion from './accordion';
 
-export { Order, Menu, Transform, ScrollTrigger, Morphing }
+export { Order, Menu, Transform, ScrollTrigger, Morphing, Accordion }
