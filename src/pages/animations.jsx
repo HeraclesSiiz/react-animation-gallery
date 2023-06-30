f4144045d3c165a70c9916768f5cb3fda068db63
Andreisc0912 <adreisc912@gmail.com>
2023-06-30 13:36:04 +0200

    add: frmaer section

---
+++ src/pages/animations.jsx
@@ -0,0 +1,71 @@
+import "./page.scss";
+import React, { useEffect, useState, useRef } from 'react';
+
+import { Grid, Typography } from "@mui/material";
+
+import {
+    Order,
+    Menu,
+    Transform,
+    ScrollTrigger,
+    Morphing,
+    Accordion,
+    Notification,
+    Drawing,
+    DrawingText
+} from "./components/framermotions";
+
+import { useInView } from "framer-motion";
+
+function Section( props) {
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
+
+    console.log(props)
+
+    return (
+        <section {...props} ref={ref}>
+            <span
+                style={{
+                    width:'100%',
+                    transform: isInView ? "none" : "translateX(-200px)",
+                    opacity: isInView ? 1 : 0,
+                    transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+                }}
+            >
+                {props.children}
+            </span>
+        </section>
+    );
+}
+
+export default function Animations(props) {
+    return (
+        <>
+            <Menu />
+            <Section className="drawing">
+                <Grid container direction={"column"}>
+                    <Grid item>
+                        <DrawingText />
+                    </Grid>
+                    <Grid item>
+                        <Drawing />
+                    </Grid>
+                </Grid>
+            </Section>
+            <Section className="transform">
+                <Transform />
+            </Section>
+            <Section className="accordion">
+                <Accordion />
+            </Section>
+            <Section className="scrolltrigger">
+                <ScrollTrigger />
+            </Section>
+            {/* <Section className="morphing">
+                <Morphing />
+            </Section> */}
+            <Notification />
+        </>
+    );
+}
