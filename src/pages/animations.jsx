992cd1fb4f332cfe1c8c7d0c01fcd8fc62187ecf
heraclessiiz <heracles2756@gmail.com>
2023-07-03 16:45:26 +0200

    show: framer logo

--- src/pages/animations.jsx
+++ src/pages/animations.jsx
@@ -43,7 +43,7 @@ export default function Animations(props) {
     return (
         <>
             <Sidebar />
-            {/* <SvgLogo /> */}
+            <SvgLogo />
             <Section className="drawing">
                 <Grid container direction={"column"}>
                     <Grid item>
